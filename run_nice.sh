62c74eea197f2d1004250bf586a4a28c7be72722
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2018-01-11 20:21:31 -0800

    Change run scripts to absolute paths for safety

--- run_nice.sh
+++ run_nice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su
-cd ../../../../../startup/wd/xmr-stak/bin/
+cd /mnt/batch/tasks/startup/wd/xmr-stak/bin/
 curl https://raw.githubusercontent.com/JonathanGawrych/cryptonight-mining-setup/master/config_template.txt > config.txt
 sudo sed -i 's/POOLURLANDPORT/cryptonight.usa.nicehash.com:3355/' config.txt
 sudo sed -i 's/WALLETADDRESS/3Ka3BgD6DyHhse4HdcSfqxAsptKaXN3LhF/' config.txt
--- run_xmr.sh
+++ run_xmr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su
-cd ../../../../../startup/wd/xmr-stak/bin/
+cd /mnt/batch/tasks/startup/wd/xmr-stak/bin/
 curl https://raw.githubusercontent.com/JonathanGawrych/cryptonight-mining-setup/master/config_template.txt > config.txt
 sudo sed -i 's/POOLURLANDPORT/pool.minexmr.com:7777/' config.txt
 sudo sed -i 's/WALLETADDRESS/45VShUov9o2dKyyXAXxXZkL2LDb9mwcpoAAfPPjJHNKcADU8J6hq57ZiijB1DSGMfn3uJz5URn16TZGw9p74sKjoFRGDcJ1/' config.txt
