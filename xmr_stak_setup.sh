d9434c2d61f33704d00ddef6084e253ad6d70910
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2018-01-11 20:25:35 -0800

    Use sudo su at the top of the setup

--- xmr_stak_setup.sh
+++ xmr_stak_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git nano
+sudo su
+apt-get --assume-yes update
+apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git nano
 git clone https://github.com/fireice-uk/xmr-stak.git
 cd xmr-stak/
 echo -e "#pragma once\nconstexpr double fDevDonationLevel = 0;" > xmrstak/donate-level.hpp
@@ -8,4 +9,4 @@ cmake . -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin/
 curl https://raw.githubusercontent.com/JonathanGawrych/cryptonight-mining-setup/master/cpu.txt > cpu.txt
-sudo sysctl -w vm.nr_hugepages=128
+sysctl -w vm.nr_hugepages=128
