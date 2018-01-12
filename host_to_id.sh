8062744282959ded109cc7baa8bcf585bee8777f
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2018-01-11 21:28:09 -0800

    Immediately follow the log on login

--- host_to_id.sh
+++ host_to_id.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export PROMPT_COMMAND='export PS1="\[\e]0;\u@NODEID: \w\a\]${debian_chroot:+($debian_chroot)}\u@NODEID:\w\$ "'
-cd /mnt/batch/tasks/startup/wd/xmr-stak/bin/
+cd /mnt/batch/tasks/startup/wd/
+cd xmr-stak/bin/ && less +F xmr.log
