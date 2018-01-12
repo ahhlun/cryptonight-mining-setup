ff07396aee2b9e601f06625b0646f8ffff9796f7
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2018-01-12 01:17:42 -0800

    Change title on ssh entry before lessing the log

--- host_to_id.sh
+++ host_to_id.sh
@@ -2,4 +2,6 @@
 
 export PROMPT_COMMAND='export PS1="\[\e]0;\u@NODEID: \w\a\]${debian_chroot:+($debian_chroot)}\u@NODEID:\w\$ "'
 cd /mnt/batch/tasks/startup/wd/
-cd xmr-stak/bin/ && less +F status.log
+cd xmr-stak/bin/
+echo -ne "\033]0;$USER@NODEID: $PWD\007"
+less +F status.log
